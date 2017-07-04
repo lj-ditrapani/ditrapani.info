4338814b67a83ff6a6c90992e305bfe9541c9c02
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2017-07-04 15:51:00 -0400

    Set scalatra environment based on java system property

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,11 @@
 import info.ditrapani._
-import org.scalatra._
+import org.scalatra.{EnvironmentKey, LifeCycle}
 import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
+    val env = util.Properties.propOrElse("info.ditrapani.environment", "development")
+    context.setInitParameter(EnvironmentKey, env)
     context.mount(new ditrapani, "/*")
   }
 }
